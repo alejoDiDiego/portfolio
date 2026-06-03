d7779ea00880837dc969f0d0b4b7fe5968499b40
Alejo Di Diego <alejohernandidiego@gmail.com>
2026-06-03 12:19:39 -0300

    (fix): responsive experience

--- frontend/src/hoc/SectionWrapper.tsx
+++ frontend/src/hoc/SectionWrapper.tsx
@@ -13,7 +13,7 @@ const SectionWrapper = (
         variants={staggerContainer()}
         initial="hidden"
         whileInView="show"
-        viewport={{ once: true, amount: 0.25 }}
+        viewport={{ once: true, amount: 0.1 }}
         className="sm:px-16 px-6 sm:py-16 py-10 max-w-4xl flex justify-center  relative z-0"
       >
         <span className="hash-span" id={idName}>
